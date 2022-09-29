195ba7af863af87712ebe9db9f9555932157f245
ladyJane1999 <anzhela.romanova.99@bk.ru>
2022-09-29 12:54:38 +0300

    Добавьте файлы проекта.

---
+++ Helper1/Helper1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.ExceptionServices;
+
+public static class Helper1
+{
+    public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+        using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
+        {
+            if (task != await Task.WhenAny(task, tcs.Task))
+            {
+                throw new TaskCanceledException(task);
+            }
+        }
+        return task.Result;
+    }
+  
+
+}
+
+
---
+++ Helper1/Program.cs
@@ -0,0 +1,85 @@
+﻿using static Helper1;
+class Program
+{
+  public static async Task Main(string[] args)
+    {
+        try
+        {
+            var tasks = new List<Task<string>>();
+            for (var i = 1; i < 4; i++)
+            {
+                var iteration = i;
+                Task<string> task = Task.Run(async () =>
+                {
+                    await Task.Delay(100);
+                    Console.WriteLine($"Iteration {iteration}");
+                    return "iteration";
+                });
+                tasks.Add(task);
+
+            }
+
+           var cancel = new CancellationTokenSource(3000);
+            var ct = cancel.Token;
+            foreach (var task in tasks)
+                await task.WithCancellation(ct);
+
+            Console.WriteLine("Task completed");
+        }
+        catch (OperationCanceledException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        /* Task<string> t1 = new Task<string>(
+        () => {
+            return "1000";
+        });
+         Task<string> t2 = new Task<string>(
+      () => {
+          return "2000";
+      });
+         Task<string> t3 = new Task<string>(
+     () => {
+         return "3000";
+     });
+
+         var tasks = new List<Task<string>>();
+         for (var i = 1; i < 4; i++)
+         {
+             var iteration = i;
+             Task<string> task = Task.Run<string>(async () =>
+             {
+                 await Task.Delay(100);
+                 Console.WriteLine($"Iteration {iteration}");
+                 return "iteration";
+             });  
+             tasks.Add(task);
+
+         }
+         //Task<string>[] tasks = new Task<string>[2];
+         try
+         {
+             CancellationToken cancellationToken = new CancellationToken();
+             foreach (var task in tasks)
+             await Helper1.WithCancellation(task, cancellationToken);
+
+         }
+         catch (Exception ex)
+         {
+             Console.WriteLine(ex.ToString());
+         }
+         //Thread.Sleep(5000);*/
+        /*   var cancel = new CancellationTokenSource(3000);
+            var ct = cancel.Token;
+
+            try
+            {
+                await Task.Delay(1000).WithCancellation(ct);
+                Console.WriteLine("Task completed");
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }*/
+    }
+}
